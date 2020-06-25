d13a820026d470a3221b2d09e5a057d4bc2da2f0
Gursimran Kaur Saini <sainigursimrankaur@gmail.com>
2020-06-25 23:05:17 +0530

    Updates

---
+++ precache-manifest.253f0375483d16cd8897e5586587ce9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd577ead1f750f1f4d9638c92f40f07",
+    "url": "/github-user-search/index.html"
+  },
+  {
+    "revision": "661f243ff8935e95b099",
+    "url": "/github-user-search/static/css/main.94074e78.chunk.css"
+  },
+  {
+    "revision": "a7677ec5b01a012ac21c",
+    "url": "/github-user-search/static/js/2.7e361851.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/github-user-search/static/js/2.7e361851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661f243ff8935e95b099",
+    "url": "/github-user-search/static/js/main.36ddca50.chunk.js"
+  },
+  {
+    "revision": "01c312be309de6103f80",
+    "url": "/github-user-search/static/js/runtime-main.13ee702e.js"
+  }
+]);
